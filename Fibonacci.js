7cbadeeb689a69a90ab41903bb372f9530a69c37
Amit Iluz <iluzzamit@gmail.com>
2019-02-23 00:27:36 +0200

    /* * Created by Amit Iluz. * Fibonacci is a self executing function that * compute the next number of fibonacci series * without any parameters from the outside. */

---
+++ Fibonacci.js
@@ -0,0 +1,21 @@
+/*
+* Created by Amit Iluz.
+* Fibonacci is a self executing function that
+* compute the next number of fibonacci series
+* without any parameters from the outside.
+*/
+
+const fibonacci = (() => {
+    const state = {fib1: 1, fib2: 0};
+    return () => {
+        state.fib2 += state.fib1;
+        state.fib1 = state.fib2 - state.fib1;
+        console.log(state.fib1);
+    }
+})();
+
+//main scope
+const N = 20;
+for (let i = 0; i < N; i++) {
+    fibonacci();
+}
